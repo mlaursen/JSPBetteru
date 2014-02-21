583f2019a32ecf81fef3411bfc996140b922e302
mlaursen <mlaursen03@gmail.com>
2014-02-21 15:45:48 -0500

    added getters and setters and toString

--- src/com/betteru/meals/database/MealPart.java
+++ src/com/betteru/meals/database/MealPart.java
@@ -3,6 +3,7 @@
  */
 package com.betteru.meals.database;
 
+import com.betteru.utils.StringNumberFormat;
 import com.github.mlaursen.annotations.DatabaseField;
 import com.github.mlaursen.annotations.DatabaseFieldType;
 import com.github.mlaursen.database.objects.DatabaseObject;
@@ -57,4 +58,87 @@ public class MealPart extends DatabaseObject implements Createable, Getable, Upd
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @return the mealId
+	 */
+	public String getMealId() {
+		return mealId;
+	}
+
+	/**
+	 * @param mealId the mealId to set
+	 */
+	public void setMealId(String mealId) {
+		this.mealId = mealId;
+	}
+	
+	public void setMealId(MyResultRow r) {
+		this.mealId = r.get("mealid");
+	}
+
+	/**
+	 * @return the ingredientId
+	 */
+	public String getIngredientId() {
+		return ingredientId;
+	}
+
+	/**
+	 * @param ingredientId the ingredientId to set
+	 */
+	public void setIngredientId(String ingredientId) {
+		this.ingredientId = ingredientId;
+	}
+	
+	public void setIngredientId(MyResultRow r) {
+		this.ingredientId = r.get("ingredientid");
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	public void setAmount(MyResultRow r) {
+		this.amount = StringNumberFormat.attemptParseDouble(r, "amount");
+	}
+
+	/**
+	 * @return the defaultUnit
+	 */
+	public String getDefaultUnit() {
+		return defaultUnit;
+	}
+
+	/**
+	 * @param defaultUnit the defaultUnit to set
+	 */
+	public void setDefaultUnit(String defaultUnit) {
+		this.defaultUnit = defaultUnit;
+	}
+
+	public void setDefaultUnit(MyResultRow r) {
+		this.defaultUnit = r.get("default_unit");
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "MealPart [" + (primaryKey != null ? "primaryKey=" + primaryKey + ", " : "")
+				+ (mealId != null ? "mealId=" + mealId + ", " : "") + (ingredientId != null ? "ingredientId=" + ingredientId + ", " : "")
+				+ "amount=" + amount + ", " + (defaultUnit != null ? "defaultUnit=" + defaultUnit : "") + "]";
+	}
+	
+	
 }
