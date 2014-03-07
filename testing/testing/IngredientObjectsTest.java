609356c1ea6d573d366ae546250525e8c75bb6c2
mlaursen <mlaursen03@gmail.com>
2014-03-07 16:02:35 -0500

    Added ingredient tests

--- testing/testing/IngredientObjectsTest.java
+++ testing/testing/IngredientObjectsTest.java
@@ -3,10 +3,9 @@
  */
 package testing;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.ClassRule;
@@ -25,16 +24,17 @@ import com.betteru.ingredients.Serving;
 import com.betteru.ingredients.objects.Ingredient;
 import com.github.mlaursen.database.managers.TestingObjectManager;
 
-
 /**
  * @author mikkel.laursen
- *
+ * 
  */
 public class IngredientObjectsTest {
+	
 	protected static TestingObjectManager tom;
 	
 	@ClassRule
 	public static ExternalResource resource = new ExternalResource() {
+		
 		@Override
 		protected void before() {
 			boolean debug = false;
@@ -46,43 +46,63 @@ public class IngredientObjectsTest {
 			tom.addPackage(Category.class);
 			tom.recompile();
 		}
+		
 		@Override
 		protected void after() {
 			tom.cleanUp();
 		}
 	};
 	
-	public Ingredient getTestIngredient() {
-		String name = "Test Ingredient";
-		Brand brand = new Brand("Test Brand");
-		Category catg = new Category("Proteins");
-		Serving def = new Serving(22, new FoodUnit("g"));
-		Serving alt = new AltServing(30, new FoodUnit("other"));
-		Calorie calorie = new Calorie(400);
-		Fat fat = new Fat(10);
-		Carbohydrate carbs = new Carbohydrate(33);
-		Protein protein = new Protein(30);
-		return new Ingredient(name, brand, catg, def, alt, calorie, fat, carbs, protein);
+	protected Ingredient getTestIngredient() {
+		return getTestIngredient("Test Ingredient", "Test Brand", "Proteins", 22, "g", 30, "other", 400, 10, 33, 30);
+	}
+	
+	protected Ingredient getTestIngredient(String name, String brand, String category, double s1, String u1, double s2, String u2,
+			double cal, double fat, double carb, double pro) {
+		return new Ingredient(name, new Brand(brand), new Category(category), new Serving(s1, new FoodUnit(u1)), new AltServing(s2,
+				new FoodUnit(u2)), new Calorie(cal), new Fat(fat), new Carbohydrate(carb), new Protein(pro));
 	}
 	
 	@Test
-	public void testCreateIngredient() {
+	public void testCreateAndGetIngredient() {
 		Ingredient i = getTestIngredient();
 		assertTrue(tom.create(i));
-	}
-
-	@Test
-	public void testGetIngredient() {
-		Ingredient i = tom.get(0, Ingredient.class);
+		i = tom.get(0, Ingredient.class);
 		assertNotNull(i);
 	}
 	
 	@Test
-	public void testGetAllIngredient() {
-		List<Ingredient> all = tom.getAll(Ingredient.class);
-		assertNotNull(all);
-		assertEquals(1, all.size());
-		assertEquals(getTestIngredient(), all.get(0));
+	public void testCreateAndGetMultiplieIngredient() {
+		String[] catgs = {"Proteins", "Carbs", "Dairy", "Other" };
+		List<Ingredient> ings = new ArrayList<Ingredient>();
+		for(int i = 0; i < 20; i++) {
+			String brand = "Test Brand" + (i % 5);
+			String catg = catgs[i%catgs.length];
+			Ingredient ing = getTestIngredient("Test Ingredient"+i, brand, catg, 22, "g", 30, "other", 400, 10, 33, 30);
+			ings.add(ing);
+			assertTrue(tom.create(ing));
+		}
+		List<Ingredient> fromDB = tom.getAll(Ingredient.class);
+		for(Ingredient i : fromDB) {
+			assertTrue(ings.contains(i));
+		}
 	}
 	
+	@Test
+	public void testFilterIngredient() {
+		List<Ingredient> allTestBrand1 = tom.filter(Ingredient.class, null, "Test Brand1");
+		for(Ingredient i : allTestBrand1) {
+			assertTrue(i.getBrand().equals(new Brand("Test Brand1")));
+		}
+		
+		List<Ingredient> allProteins = tom.filter(Ingredient.class, "Proteins", null);
+		for(Ingredient i : allProteins) {
+			assertTrue(i.getCategory().equals(new Category("Proteins")));
+		}
+		
+		List<Ingredient> allProteinBrand1 = tom.filter(Ingredient.class, "Proteins", "Test Brand1");
+		for(Ingredient i : allProteinBrand1) {
+			assertTrue(i.getBrand().equals(new Brand("Test Brand1")) && i.getCategory().equals(new Category("Proteins")));
+		}
+	}
 }
