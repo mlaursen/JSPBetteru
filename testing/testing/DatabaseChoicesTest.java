dc9dbf0e99744658698f9aa71cc1dc2959ab7f4f
mlaursen <mlaursen03@gmail.com>
2014-03-07 11:48:33 -0500

    added mroe tests for brands/category/foodunit

--- testing/testing/DatabaseChoicesTest.java
+++ testing/testing/DatabaseChoicesTest.java
@@ -5,10 +5,21 @@ package testing;
 
 import static com.betteru.databasechoices.accounts.Gender.FEMALE;
 import static com.betteru.databasechoices.accounts.Gender.MALE;
-import static com.betteru.databasechoices.accounts.Multiplier.*;
-import static com.betteru.databasechoices.accounts.Weekday.*;
-import static com.betteru.databasechoices.accounts.UnitSystem.*;
-import static org.junit.Assert.assertEquals;
+import static com.betteru.databasechoices.accounts.Multiplier.EXTREMELY_ACTIVE;
+import static com.betteru.databasechoices.accounts.Multiplier.LIGHTLY_ACTIVE;
+import static com.betteru.databasechoices.accounts.Multiplier.MODERATELY_ACTIVE;
+import static com.betteru.databasechoices.accounts.Multiplier.SEDENTARY;
+import static com.betteru.databasechoices.accounts.Multiplier.VERY_ACTIVE;
+import static com.betteru.databasechoices.accounts.UnitSystem.IMPERIAL;
+import static com.betteru.databasechoices.accounts.UnitSystem.METRIC;
+import static com.betteru.databasechoices.accounts.Weekday.FRIDAY;
+import static com.betteru.databasechoices.accounts.Weekday.MONDAY;
+import static com.betteru.databasechoices.accounts.Weekday.SATURDAY;
+import static com.betteru.databasechoices.accounts.Weekday.SUNDAY;
+import static com.betteru.databasechoices.accounts.Weekday.THURSDAY;
+import static com.betteru.databasechoices.accounts.Weekday.TUESDAY;
+import static com.betteru.databasechoices.accounts.Weekday.WEDNESDAY;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,29 +32,39 @@ import com.betteru.databasechoices.accounts.Gender;
 import com.betteru.databasechoices.accounts.Multiplier;
 import com.betteru.databasechoices.accounts.UnitSystem;
 import com.betteru.databasechoices.accounts.Weekday;
+import com.betteru.databasechoices.ingredients.Brand;
+import com.betteru.databasechoices.ingredients.Category;
+import com.betteru.databasechoices.ingredients.FoodUnit;
 import com.github.mlaursen.bootstrap.forms.fields.DropdownChoice;
 import com.github.mlaursen.database.managers.TestingObjectManager;
 
 /**
  * @author mikkel.laursen
- *
+ * 
  */
 public class DatabaseChoicesTest {
+	
 	protected static TestingObjectManager tom;
 	
 	@ClassRule
 	public static ExternalResource resource = new ExternalResource() {
+		
 		@Override
 		protected void before() {
 			boolean debug = false;
 			boolean delete = true;
 			boolean copyData = true;
-			tom = new TestingObjectManager(delete, debug, copyData, "gender", "weekday", "unit_system", "multiplier");
+			tom = new TestingObjectManager(delete, debug, copyData, "gender", "weekday", "unit_system", "multiplier", "category", "brand",
+					"ingredient", "food_unit");
 			tom.addPackage(Gender.class);
 			tom.addPackage(Weekday.class);
 			tom.addPackage(Multiplier.class);
 			tom.addPackage(UnitSystem.class);
+			tom.addPackage(Category.class);
+			tom.addPackage(Brand.class);
+			tom.addPackage(FoodUnit.class);
 		}
+		
 		@Override
 		protected void after() {
 			tom.cleanUp();
@@ -65,7 +86,7 @@ public class DatabaseChoicesTest {
 		List<Multiplier> multipliers = Arrays.asList(SEDENTARY, LIGHTLY_ACTIVE, MODERATELY_ACTIVE, VERY_ACTIVE, EXTREMELY_ACTIVE);
 		assertEquals(multipliers.size(), multipliersDB.size());
 		List<DropdownChoice> choices = new Multiplier().getAllChoices();
-		assertEquals(multipliers.size()+1, choices.size());
+		assertEquals(multipliers.size() + 1, choices.size());
 	}
 	
 	@Test
@@ -84,71 +105,67 @@ public class DatabaseChoicesTest {
 		List<UnitSystem> units = Arrays.asList(IMPERIAL, METRIC);
 		assertEquals(units.size(), unitsDB.size());
 		List<DropdownChoice> choices = new UnitSystem().getAllChoices();
-		assertEquals(units.size()+1, choices.size());
-	}
-/*
-	@Test
-	public void testAccountChoices() {
-		Gender g = new Gender("MALE");
-		assertEquals(g, g);
-		List<Gender> genders = g.getAll(Gender.class);
-		assertTrue(genders.contains(new Gender("MALE")));
-		assertTrue(genders.contains(new Gender("FEMALE")));
-		assertEquals(2, genders.size());
-		
-		Multiplier m = new Multiplier();
-		Multiplier sedentary = m.get("SEDENTARY", Multiplier.class);
-		List<Multiplier> multipliers = m.getAll(Multiplier.class);
-		assertEquals(5, multipliers.size());
-		assertTrue(multipliers.contains(new Multiplier("SEDENTARY")));
-		assertTrue(multipliers.contains(new Multiplier("LIGHTLY ACTIVE")));
-		assertTrue(multipliers.contains(new Multiplier("VERY ACTIVE")));
-		assertTrue(multipliers.contains(new Multiplier("EXTREMELY ACTIVE")));
-		assertTrue(multipliers.contains(new Multiplier("MODERATELY ACTIVE")));
-		assertEquals(1.2, sedentary.getAmount(), 1);
-		
-		UnitSystem us = new UnitSystem();
-		List<UnitSystem> units = us.getAll(UnitSystem.class);
-		assertEquals(2, units.size());
-		assertTrue(units.contains(new UnitSystem("IMPERIAL")));
-		assertTrue(units.contains(new UnitSystem("METRIC")));
-		
-		Weekday w = new Weekday();
-		Weekday monday = w.get("MONDAY", Weekday.class);
-		List<Weekday> ws = w.getAll(Weekday.class);
-		assertEquals(7, ws.size());
-		assertTrue(ws.contains(monday));
-		assertTrue(ws.contains(new Weekday("TUESDAY")));
-		assertTrue(ws.contains(new Weekday("WEDNESDAY")));
-		assertTrue(ws.contains(new Weekday("THURSDAY")));
-		assertTrue(ws.contains(new Weekday("FRIDAY")));
-		assertTrue(ws.contains(new Weekday("SATURDAY")));
-		assertTrue(ws.contains(new Weekday("SUNDAY")));
-		assertEquals(1, monday.getDayOfWeek());
+		assertEquals(units.size() + 1, choices.size());
 	}
 	
 	@Test
-	public void testIngredientChoices() {
-		Brand b = new Brand();
-		Brand testBrand = new Brand("Test");
-		//System.out.println(testBrand.create());
-		assertTrue(testBrand.create());
-		List<Brand> bs = b.getAll(Brand.class);
-		assertTrue(bs.contains(testBrand));
-		assertTrue(testBrand.delete());
-		
-		Category c = new Category();
-		List<Category> cs = c.getAll(Category.class);
-		assertEquals(4,  cs.size());
-		assertTrue(cs.contains(new Category("Proteins")));
-		assertTrue(cs.contains(new Category("Carbs")));
-		assertTrue(cs.contains(new Category("Dairy")));
-		assertTrue(cs.contains(new Category("Other")));
-		
-		FoodUnit fu = new FoodUnit();
-		FoodUnit gram = fu.get("g", FoodUnit.class);
-		assertEquals("g", gram.getPrimaryKey());
-		assertEquals("gram", gram.getLongName());
+	public void testBrand() {
+		List<Brand> bsDB = tom.getAll(Brand.class);
+		List<DropdownChoice> choices = new Brand().getAllChoices();
+		assertEquals(bsDB.size() + 1, choices.size());
+		assertTrue(choices.contains(new Brand("New Brand")));
 	}
-*/
+	
+	@Test
+	public void testCreateBrand() {
+		Brand testing = new Brand("Testing");
+		assertTrue(tom.create(testing));
+		List<Brand> bs = tom.getAll(Brand.class);
+		assertTrue(bs.contains(testing));
+	}
+	
+	@Test
+	public void testCategory() {
+		List<Category> csDB = tom.getAll(Category.class);
+		List<DropdownChoice> choices = new Category().getAllChoices();
+		assertEquals(csDB.size(), choices.size());
+		assertTrue(csDB.contains(new Category("Proteins")));
+		assertTrue(csDB.contains(new Category("Carbs")));
+		assertTrue(csDB.contains(new Category("Dairy")));
+		assertTrue(csDB.contains(new Category("Other")));
+	}
+	
+	@Test
+	public void testCreateCategory() {
+		Category c = new Category("CAN't");
+		assertFalse(tom.create(c));
+	}
+	
+	@Test
+	public void testDeleteCategory() {
+		assertFalse(tom.delete(new Category("Proteins")));
+	}
+	
+	@Test
+	public void testFoodUnit() {
+		List<FoodUnit> unitsDB = tom.getAll(FoodUnit.class);
+		List<DropdownChoice> choices = new FoodUnit().getAllChoices();
+		assertEquals(unitsDB.size(), choices.size());
+		FoodUnit g = tom.get("g", FoodUnit.class);
+		assertNotNull(g);
+		assertEquals("g", g.getPrimaryKey());
+		assertEquals("gram", g.getLongName());
+	}
+	
+	@Test
+	public void testDeleteFoodUnit() {
+		FoodUnit g = tom.get("g", FoodUnit.class);
+		assertFalse(tom.delete(g));
+	}
+	
+	@Test
+	public void testCreateFoodUnit() {
+		FoodUnit g = new FoodUnit("blob");
+		assertFalse(tom.create(g));
+	}
 }
