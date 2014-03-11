a98df24ff5dd5540f7749b23701c7d55426bb0f1
mlaursen <mlaursen03@gmail.com>
2014-03-10 22:29:36 -0400

    working on stuffs

--- src/com/betteru/intake/objects/DailyIntake.java
+++ src/com/betteru/intake/objects/DailyIntake.java
@@ -4,9 +4,13 @@
 package com.betteru.intake.objects;
 
 import java.sql.Date;
+import java.util.Arrays;
+import java.util.List;
+
 import com.github.mlaursen.annotations.DatabaseField;
 import com.github.mlaursen.annotations.DatabaseFieldType;
 import com.github.mlaursen.database.objects.MyResultRow;
+import com.github.mlaursen.database.objects.Procedure;
 import com.github.mlaursen.database.procedures.Createable;
 import com.github.mlaursen.database.procedures.Filterable;
 import com.github.mlaursen.database.procedures.Updateable;
